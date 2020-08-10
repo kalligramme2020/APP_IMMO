f6f91e1d3d8d3fffa9a7def55ca023cb06969b2a
blondel <stephanemekontcho97u@gmail.com>
2020-08-10 23:12:14 +0200

    modifier ++++

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravellg,elr,gle
+                    Laravel
                 </div>
 
                 <div class="links">
